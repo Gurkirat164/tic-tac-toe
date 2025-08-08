75128945fdd3ab9a187cb4a7359173ec93ec0511
Gurkirat164 <ggurkiratssingh1@gmail.com>
2025-08-08 16:44:59 +0530

    Added draw and popup functions

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 let gamebox = document.querySelectorAll(".game-box");
-let resetbtn = document.querySelector(".reset-button");
-let newgamebtn = document.querySelector(".new-game-button");
+let resetbtn = document.querySelectorAll(".reset-button");
+let newgamebtn = document.querySelectorAll(".new-game-button");
 let xwincount = document.querySelector(".score-x");
 let owincount = document.querySelector(".score-o");
+let draw_popup = document.querySelector("#draw-popup");
+let win_popup = document.querySelector("#win-popup");
+let close_btn = document.querySelectorAll(".close-button");
+let winner = document.querySelector(".winner");
 
 let turnx = true;
 let winx = 0;
 let wino = 0;
+
+let turn_no = 0;
 
 xwincount.innerText = winx;
 owincount.innerText = wino;
@@ -27,16 +33,55 @@ gamebox.forEach((box) => {
             if (turnx) {
                   box.innerText = "X";
                   turnx = false;
+                  turn_no++;
             }
             else {
                   box.innerText = "O";
                   turnx = true;
+                  turn_no++;
             }
             box.disabled = true;
             checkWin();
+            checkdraw();
 
       })
 })
+
+resetbtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+            resetGame();
+            unlockall();
+            turnx = true;
+            turn_no = 0;
+            draw_popup.style.display = "none";
+            win_popup.style.display = "none";
+            
+      });
+});
+
+newgamebtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+            resetGame();
+            unlockall();
+            winx = 0;
+            wino = 0;
+            turn_no = 0;
+            updateScore();
+            turnx = true;
+            draw_popup.style.display = "none";
+            win_popup.style.display = "none";
+            gamebox.forEach((box) => {
+                  box.style.backgroundColor = "";
+            })
+      })
+});
+
+close_btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+            draw_popup.style.display = "none";
+            win_popup.style.display = "none";
+    });
+});
 
 const lockall = () => {
       for (let box of gamebox) {
@@ -62,6 +107,13 @@ const updateScore = () => {
       owincount.innerText = wino;
 }
 
+const checkdraw = () => {
+      if (turn_no === 9) {
+            lockall();
+            draw_popup.style.display = "block";
+      }
+}
+
 const checkWin = () => {
       for (let condition of win_conditions) {
             let pos1val = gamebox[condition[0]].innerText;
@@ -72,7 +124,9 @@ const checkWin = () => {
                   if (pos1val == pos2val && pos2val == pos3val) {
                         gamebox[condition[0]].style.backgroundColor = "green";
                         gamebox[condition[1]].style.backgroundColor = "green";
-                        gamebox[condition[2]].style.backgroundColor = "green";      
+                        gamebox[condition[2]].style.backgroundColor = "green";
+                        winner.innerText = pos1val;
+                        win_popup.style.display = "block";
                         lockall();
                         if (pos1val == "X") {
                               winx++;
@@ -80,29 +134,9 @@ const checkWin = () => {
                         }
                         else {
                               wino++;
-                              console.log(wino + "O wins!");
                               updateScore();
                         }
                   }
             }
       }
 }
-
-resetbtn.addEventListener("click", () => {
-      resetGame();
-      unlockall();
-      turnx = true;
-})
-
-newgamebtn.addEventListener("click", () => {
-      resetGame();
-      unlockall();
-      winx = 0;
-      wino = 0;
-      updateScore();
-      turnx = true;
-      gamebox.forEach((box) => {
-            box.style.backgroundColor = "";
-      })
-})
-
